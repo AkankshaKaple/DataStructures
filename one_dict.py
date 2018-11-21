48aa3d9d3258242f85d39ea77934a0da4dfbef66
AkankshaKaple <akankshakaple.gmail.com>
2018-11-21 17:50:55 +0530

    Second Commit

--- one_dict.py
+++ one_dict.py
@@ -2,19 +2,26 @@
 # dic2={3:30, 4:40}
 # dic3={5:50,6:60}
 # a = {}
-# merge(dic1,dic2)
+# dic1.update(dic2)
 # dic1.update(dic3)
 # print(dic1)
 dict = {}
+dict1={}
 list = []
 num = int(input("ENter number of divtionaries you want to concatinate:"))
-for i in range(num) :
-    while num > 0:
-        size = int(input("Enter number of key value pairs you want in this dict : "))
-        while size > 0:
-            input_key = input("Give key to add onto the dictionary : ")
-            input_value = input("Give value to add onto the dictionary : ")
-            dict[input_key] = input_value
-            print(dict)
-            size-=1
-        num-=1
+while num > 0:
+    size = int(input("Enter number of key value pairs you want in this dict : "))
+    while size > 0:
+
+                input_key = input("Give key to add onto the dictionary : ")
+                input_value = input("Give value to add onto the dictionary : ")
+                dict[input_key] = input_value
+                list.append(dict.items())
+                # print(dict)
+                size-=1
+
+
+    num-=1
+# print(list)
+    dict.update(dict1)
+print(dict)

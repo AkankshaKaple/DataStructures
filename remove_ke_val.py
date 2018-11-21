987e8b950f7acb7e1dc72e2d8c13d27f1ab2c66b
AkankshaKaple <akankshakaple.gmail.com>
2018-11-21 18:12:02 +0530

    Second Commit

---
+++ remove_ke_val.py
@@ -0,0 +1,6 @@
+# This program deletes the key from directory
+dict = {'1' : 'Aknaksha', '2' : 'Siddhesh' , '3' : 'Sampada', '4' : 'Varad'}
+print(dict)
+key = input("Enter the key you want to remove : ")
+dict.pop(key)
+print(dict)

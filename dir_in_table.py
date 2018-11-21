9295f4d1a15cfbffff2862e54455f1e433088fde
AkankshaKaple <akankshakaple.gmail.com>
2018-11-21 18:51:28 +0530

    Second Commit

---
+++ dir_in_table.py
@@ -0,0 +1,4 @@
+# This program prints the dictionary in table format
+dict = {'1' : 'Aknaksha', '2' : 'Siddhesh' , '3' : 'Sampada', '4' : 'Varad'}
+for key, value in dict.items() :
+    print(key,value)
